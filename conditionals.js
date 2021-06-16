c5b6a18e7a33e3e8a1d9e18def1f7cd6073256ce
CGstyling <christinaguseva@gmail.com>
2021-06-16 12:12:59 +0200

    Exercise conditionals and switch statements

--- conditionals.js
+++ conditionals.js
@@ -8,24 +8,32 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
 // ==========================================
-
+const equalNumbers1 = 10 > 20;
+console.log("is tien groter dan 20? ", equalNumbers1);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
 // ==========================================
-
+const notEqualNumbers = 10 != 11;
+console.log("Is 10 ongelijk aan 11? ", notEqualNumbers)
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false wanneer gelogd
 // ==========================================
-
+const sweet = "zoet";
+const juicy = "sappig";
+const notEqualNumbers3 = sweet === juicy;
+console.log("Zijn de waardes gelijk? ", notEqualNumbers3);
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
 // 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!' wanneer gelogd
 // ==========================================
-
+const number = 9;
+if (number > 8) {
+    console.log("Cum laude!");
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
@@ -34,5 +42,12 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // Tip: verander de waarde van isRaining en temperature even handmatig om te kijken of jouw 2e conditie klopt
 // ==========================================
 
-const isRaining = false;
-const temperature = 23;
+const isRaining = true;
+const temperature = 21;
+
+if (isRaining === false && temperature >= 21) {
+    console.log("Je kan een korte broek aan")
+}
+if (isRaining === true && temperature <= 20) {
+    console.log("Doe een jas aan");
+}
--- switch-statements.js
+++ switch-statements.js
@@ -1,8 +1,15 @@
 // ==========================================
 // Opdracht 1: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
 // ==========================================
-
 const errorCode = 0;
+switch (errorCode) {
+    case 1:
+    console.log("Er zitten geen errors in de code");
+        break;
+    case 0:
+    console.log("ERROR:ERROR:ERROR:ERROR:ERROR:ERROR:ERROR:ERROR:ERROR")
+        break;
+}
 
 // Je mag ervanuit gaan dat deze variabele altijd een waarde tussen de 0 en de 4 bevat. Deze codes hebben de volgende betekenissen (foutmeldingen):
 // 0: "Geen watertoevoer"
@@ -21,7 +28,12 @@ const errorCode = 0;
 // Opdracht 2: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
 // ==========================================
 
-const monitorColor = "red";
+let monitorColor = "green";
+monitorColor = "red";
+monitorColor = "yellow";
+monitorColor = "orange";
+monitorColor = "red";
+
 
 // Deze variabele zal altijd een kleur bevatten. De specifieke kleuren waar een melding bij gemaakt moet worden zijn: "red", "green", "yellow" en "orange".
 // Deze codes zijn gelinkt aan de volgende meldingen:
@@ -30,6 +42,22 @@ const monitorColor = "red";
 // "orange": "Kritieke hartslag, actie vereist!"
 // "red": "Kritieke hartslag, actie vereist!"
 // Bij alle andere kleuren is de status van de hartslag onbekend.
+
+switch (monitorColor) {
+    case "green":
+        console.log("hartslag stabiel");
+        break;
+    case "yellow":
+        console.log("Hartslag verhoogd");
+        break;
+    case "orange":
+    case "red":
+        console.log("Kritieke hartslag, actie vereist!");
+        break;
+    default:
+        console.log("Onbekend")
+        break;
+}
 
 // Verwachtte uitkomsten:
 // const monitorColor = "red" geeft "Kritieke hartslag, actie vereist!" in de terminal
@@ -40,3 +68,13 @@ const monitorColor = "red";
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: kun je de switch-statement uit opdracht 2 ook opschrijven met een aantal if-statements?
 // ==========================================
+
+if (monitorColor === "green") {
+    console.log("hartslag stabiel");
+} else if (monitorColor === "yellow") {
+    console.log("hartslag verhoogd");
+} else if (monitorColor === "orange" || monitorColor === "red") {
+    console.log("Kritieke hartslag, actie vereist!");
+} else {
+    console.log("Alles is goed, ga door met leven")
+}
